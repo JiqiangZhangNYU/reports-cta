b1653b9fe26c7adb7001b8ded95f993fd0a1860a
JiqiangZhangNYU <zjq950711@163.com>
2026-05-28 23:38:52 +0800

    update dashboard 2026-05-28

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Auto-generated — do not edit
-const CACHE = 'oos-vebc5146d';
+const CACHE = 'oos-v999d9af5';
 const PRECACHE = ['./', './data/meta.json', './data/nav_6m.json', './data/nav_2y.json'];
 
 self.addEventListener('install', e => {
